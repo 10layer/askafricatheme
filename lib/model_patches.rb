63a27439b823a249adda9d4faea83779f059e7a7
Jason Norwood-Young <jason@10layer.com>
2014-04-25 08:43:08 +0200

    Bunch of fixes and Form A

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -5,11 +5,29 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
-    OutgoingMessage.class_eval do
-        # Add intro paragraph to new request template
-        def default_letter
-            return nil if self.message_type == 'followup'
-            #"If you uncomment this line, this text will appear as default text in every message"    
-        end
-    end        
+
+	InfoRequest.class_eval do
+		def law_used_full
+			return "Promotion of Access to Information Act"
+		end
+	    def law_used_short
+	    	return "PAIA"
+	    end
+	    def law_used_act
+	    	return "Promotion of Access to Information Act"
+	    end
+	    def law_used_with_a
+	       return "A Promotion of Access to Information Act request"
+	    end
+	end
+
+	OutgoingMessage.class_eval do
+		# Add intro paragraph to new request template
+		def default_letter
+			return File.open(File.dirname(__FILE__) + "/views/request/_default_letter.txt").read
+			# return render :partial => "default_letter"
+			# return nil if self.message_type == 'followup'
+			#"If you uncomment this line, this text will appear as default text in every message"    
+		end
+	end        
 end
