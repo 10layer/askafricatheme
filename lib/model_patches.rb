699a9d1ce0f4a1844aac546a58367f8a92259844
Jason Norwood-Young <jason@10layer.com>
2014-08-02 12:31:40 +0200

    New Form A submission and some redacting of IDs, emails and phone numbers

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -22,12 +22,49 @@ Rails.configuration.to_prepare do
 	end
 
 	OutgoingMessage.class_eval do
+
+		def contains_id?
+			/(((\d{2}((0[13578]|1[02])(0[1-9]|[12]\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\d|30)|02(0[1-9]|1\d|2[0-8])))|([02468][048]|[13579][26])0229))(( |-)(\d{4})( |-)(\d{3})|(\d{7}))/.match(self.body)
+		end
+
 		# Add intro paragraph to new request template
 		def default_letter
-			return File.open(File.dirname(__FILE__) + "/views/request/_default_letter.txt").read
-			# return render :partial => "default_letter"
+			default_letter = File.open(File.dirname(__FILE__) + "/views/request/_default_letter.txt").read
+			return default_letter
+			# return "Blah"
+			# return render plain: "Test"
 			# return nil if self.message_type == 'followup'
 			#"If you uncomment this line, this text will appear as default text in every message"    
 		end
-	end        
+
+		 # We hide emails from display in outgoing messages.
+	    def remove_privacy_sensitive_things!(text)
+	        text.gsub!(MySociety::Validate.email_find_regexp, "[Email address redacted]")
+	        text.gsub!(/(((\d{2}((0[13578]|1[02])(0[1-9]|[12]\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\d|30)|02(0[1-9]|1\d|2[0-8])))|([02468][048]|[13579][26])0229))(( |-)(\d{4})( |-)(\d{3})|(\d{7}))/, "[ID number redacted]")
+	        text.gsub!(/(\+)?([-\._\(\) ]?[\d]{3,20}[-\._\(\) ]?){2,10}/, "[Telephone number redacted]")
+	    end
+	end  
+
+	IncomingMessage.class_eval do
+		# Remove emails, mobile phones and other details FOI officers ask us to remove.
+	    def remove_privacy_sensitive_things!(text)
+	        # Remove any email addresses - we don't want bounce messages to leak out
+	        # either the requestor's email address or the request's response email
+	        # address out onto the internet
+	        text.gsub!(MySociety::Validate.email_find_regexp, "[email address]")
+
+	        # Mobile phone numbers
+	        # http://www.whatdotheyknow.com/request/failed_test_purchases_off_licenc#incoming-1013
+	        # http://www.whatdotheyknow.com/request/selective_licensing_statistics_i#incoming-550
+	        # http://www.whatdotheyknow.com/request/common_purpose_training_graduate#incoming-774
+	        text.gsub!(/(Mobile|Mob)([\s\/]*(Fax|Tel))*\s*:?[\s\d]*\d/, "[mobile number]")
+
+	        # Remove WhatDoTheyKnow signup links
+	        text.gsub!(/http:\/\/#{AlaveteliConfiguration::domain}\/c\/[^\s]+/, "[WDTK login link]")
+
+	        text.gsub!(/(((\d{2}((0[13578]|1[02])(0[1-9]|[12]\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\d|30)|02(0[1-9]|1\d|2[0-8])))|([02468][048]|[13579][26])0229))(( |-)(\d{4})( |-)(\d{3})|(\d{7}))/, "[id number]")
+	        # Remove things from censor rules
+	        self.info_request.apply_censor_rules_to_text!(text)
+	    end   
+	end   
 end
