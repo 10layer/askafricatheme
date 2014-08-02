ada386fb0ae6a06fde444f1624b9e7c32a27d0cf
Jason Norwood-Young <jason@10layer.com>
2014-08-02 12:29:34 +0200

    Hide message warning of emails in body

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -15,4 +15,27 @@ Rails.configuration.to_prepare do
         def help_out
         end
     end
+
+    RequestController.class_eval do
+        def render_new_preview
+            message = ""
+            
+            # if @outgoing_message.contains_email?
+            #     if @user.nil?
+            #         message += _("<p>You do not need to include your email in the request in order to get a reply, as we will ask for it on the next screen (<a href=\"{{url}}\">details</a>).</p>", :url => (help_privacy_path+"#email_address").html_safe);
+            #     else
+            #         message += _("<p>You do not need to include your email in the request in order to get a reply (<a href=\"{{url}}\">details</a>).</p>", :url => (help_privacy_path+"#email_address").html_safe);
+            #     end
+            #     message += _("<p>We recommend that you edit your request and remove the email address.
+            #         If you leave it, the email address will be sent to the authority, but will not be displayed on the site.</p>")
+            # end
+            if @outgoing_message.contains_postcode?
+                message += _("<p>Your request contains a <strong>postcode</strong>. Unless it directly relates to the subject of your request, please remove any address as it will <strong>appear publicly on the Internet</strong>.</p>");
+            end
+            if not message.empty?
+                flash.now[:error] = message.html_safe
+            end
+            render :action => 'preview'
+        end
+    end
 end
